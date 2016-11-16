b290d8f2016da4a529b0ac2721707eb4888c43b9
Aleksandra <Aleksandra@Lenovo-PC>
2016-11-16 16:02:07 +0100

    new comment

--- src/de/ws1617/pccl/app/Main.java
+++ src/de/ws1617/pccl/app/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//slight change
-		
+		// some comment
 		// read runtime arguments
 		try {
 			Grammar grammar = GrammarUtils.readGrammar(args[0]);
